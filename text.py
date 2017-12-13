abbdf6ba24ca4d97b668673252006f2d9c81777d
talhakhan19992 <34507072+talhakhan19992@users.noreply.github.com>
2017-12-13 15:21:05 +0500

    Create text.py

---
+++ text.py
@@ -0,0 +1,30 @@
+## @file   text.py
+##
+## @author Benjamin Williams <bwilliams@lincoln.ac.uk>
+## @brief  Contains arrays of random strings to use in certain scenarios.
+##
+
+
+#Random statuses to change to every x minutes. "Playing x"
+statuses = [
+	"a\xa0Guitar", "a\xa0Banjo", "SA-MP", "VC-MP",
+	"a\xa0Saxophone", "with\xa0your\xa0Mum", "10101010",
+	"Cards", "Poker", "Chess", "a Trumpet"
+];
+
+
+#Exit messages to pull from when quitting
+exitMessages = [
+	"Adios!", "Bye!", "Laters!", "Smell you later! :nose:", ":wave: :wave: :wave:", "*beep boop beep*\nBye bye!"
+];
+
+
+#Messages for when a result isn't fully loaded and needs some filler
+processingMessages = [
+	"*beep boop beep...*", "*101010101010...*", "*beep boop...*"
+];
+
+#When no parameters are given to a command
+usageMessages = [
+	"you silly sausage! :stuck_out_tongue:", ".. try again :grimacing:", ".. duh :rolling_eyes:", ".. silly!"
+];
